4a71c082c1cd8fb1abf69675a8900e3027e60f4a
Reece Walsh <71872899+reeceawalsh@users.noreply.github.com>
2024-07-05 15:30:34 +0100

    Changed browser router to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import PrivacyPolicyPage from './pages/privacy-policy'
 import HomePage from './pages/home'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
