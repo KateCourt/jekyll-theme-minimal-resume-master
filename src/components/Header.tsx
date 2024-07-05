989de99214221d7ec3ac01f9a75c40c115511c00
Reece Walsh <71872899+reeceawalsh@users.noreply.github.com>
2024-07-05 15:25:15 +0100

    Updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import NavLink from './NavLink'
+// import NavLink from './NavLink'
 
 const Header: React.FC = () => {
   return (
     <header className="absolute top-0 left-0 w-full flex justify-between items-center py-4 px-8 bg-transparent z-20">
       <div>
         <a
-          href="/"
+          href="https://transplant-dev.orqa.uk/"
           className="text-xl transition ease-in duration-300 md:text-3xl lg:text-4xl font-bold text-white tracking-widest leading-9 hover:text-teal-500">
           OrQA
         </a>
       </div>
       <nav>
         <ul className="flex space-x-4 md:space-x-12">
-          <li>
+          {/* <li>
             <NavLink href="/">Home</NavLink>
           </li>
           <li>
@@ -21,7 +21,7 @@ const Header: React.FC = () => {
           </li>
           <li>
             <NavLink href="/#contact">Contact</NavLink>
-          </li>
+          </li> */}
         </ul>
       </nav>
       <div></div>
